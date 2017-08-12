034615735c998e40a11601aa5c76688ce5817618
Clay Zhu <clayzhu@163.com>
2017-08-12 16:09:53 +0800

    建立项目。

---
+++ CZStringDemo/CZStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZStringDemo
+//
+//  Created by Clay Zhu on 2017/8/12.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
